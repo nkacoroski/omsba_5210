9d328ae3fbdc1a9b6d527d1242ddf3c64a4cff00
Natasha Kacoroski <nkacoroski@gmail.com>
2023-02-11 15:44:06 -0800

    Add week 6 discussion work.

---
+++ week_6_discussion.R
@@ -0,0 +1,10 @@
+# Week 6 Discussion -------------------------------------------------------
+
+
+
+# Import libraries.
+library(treemap)
+library(tidyverse)
+
+# Browse vignettes.
+browseVignettes("treemap")
